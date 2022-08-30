9fe7936ae4e75efd4bf93e991aef4f4d8706598f
berliniixi <steliosevoix@gmail.com>
2022-08-30 17:12:13 +0300

    Initial Commit

---
+++ Assets/Script/Drive.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour {
+
+    float speed = 5.0f;
+    float rotationSpeed = 100.0f;
+    Animator anim;
+
+    void Start() {
+
+        anim = GetComponent<Animator>();
+    }
+    
+    void Update() {
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Rotate(0.0f, rotation, 0.0f);
+
+        if(translation != 0) {
+
+            anim.SetBool("isWalking", true);
+        } else {
+
+            anim.SetBool("isWalking", false);
+        }
+    }
+}
